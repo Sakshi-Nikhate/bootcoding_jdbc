2dff744bd8d0e07de7c6b1f24124daea991622ab
Sakshi-Nikhate <sakshinikhate@gmail.com>
2023-08-22 19:38:55 +0530

    jdbcExamples

---
+++ src/main/java/bootcoding/jdbc_connectivity/crud/InsertjdbcExample.java
@@ -0,0 +1,33 @@
+package bootcoding.jdbc_connectivity.crud;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class InsertjdbcExample {
+    public static void main(String[] args) {
+
+        try{
+            // 1. Load JDBC Driver
+            Class.forName("org.postgresql.Driver");
+
+            // 2. Get Database Connection Object
+            Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres",
+                    "postgres", "postgres");
+
+            // 3. Query Database using Statement Object
+            Statement stmt = conn.createStatement();
+            String sql = "INSERT INTO customer VALUES(2000, 'Raju', 'Nagpur', 'raju@nagpur.com', 123)";
+            // 4. Execute Query and Get ResultSet
+            stmt.executeUpdate(sql);
+            // 4.1 Extract Result
+
+
+            // 5.Connection CLose;
+            conn.close();
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+
+}
